2ffad64113015f17c08e47951999ac5146ce3297
2ndyrGroupB <2ndyrGroupB@pn.local>
2019-10-01 16:28:53 +0800

    latest

--- scripts/controller.js
+++ scripts/controller.js
@@ -39,31 +39,3 @@ $(document).ready(function () {
 
 });
 
-
-// // advance functionalities
-// client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
-// client.subscribe("mqtt/demo", function (err){
-//   if (err){
-//     console.log(err);
-//   } else {
-//     console.log("subscribed")
-//   }
-// })
-
-// client.on("connect", function(){
-//     console.log("Successfully connected");
-// })
-
-// client.on("message", function (topic, payload) {
-//   console.log([topic, payload].join(": "));
-//   client.end();
-// })
-
-// client.publish("mqtt/demo", "hello world!", function(err){
-//   if (err){
-//     console.log(err)
-//   } else {
-//     console.log("published")
-//   }
-// })
-
